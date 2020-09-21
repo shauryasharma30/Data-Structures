588b7303ead57ce9e6b093635fe17213a0a83f2a
Shaurya <60067200+shauryasharma30@users.noreply.github.com>
2020-09-21 06:48:04 +0530

    Iterator's use in Linked List

--- Linked List/LinkedListIterativeDemonstration.java
+++ Linked List/LinkedListIterativeDemonstration.java
@@ -3,7 +3,7 @@ package com.company;
 import java.util.Iterator;
 import java.util.LinkedList;
 
-public class Main {
+class Main {
 
     public static void main(String[] args) {
         LinkedList<String> cars = new LinkedList<String>();  // a list of cars manufacturers
